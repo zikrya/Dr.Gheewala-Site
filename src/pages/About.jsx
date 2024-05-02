e7ea95d32306818c5ff675794749d8dfb0ac047e
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-01 23:52:38 -0400

    [REFRACTOR]  minor edit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Bio from '../components/AboutComponents.jsx/Bio'
 import Doctor from '../components/AboutComponents.jsx/Doctor'
+import ContactForm from '../components/ContactForm'
 
 const About = () => {
   return (
     <>
     <Bio />
     <Doctor />
+    <ContactForm />
     </>
   )
 }
