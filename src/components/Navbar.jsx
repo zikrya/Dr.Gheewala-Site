4e2c3dd9ec6414b34640048598684a68751cec14
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-02 00:45:13 -0400

    Added updates to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom'; // Import useLocation
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
+  const location = useLocation(); // Get location object
   const [hasScrolled, setHasScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleScroll = () => {
-    setHasScrolled(window.scrollY > 50);
+    // Apply scroll logic only on the homepage
+    if (location.pathname === '/') {
+      setHasScrolled(window.scrollY > 50);
+    }
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    if (location.pathname === '/') {
+      window.addEventListener('scroll', handleScroll);
+    }
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [location.pathname]); // Re-run effect when path changes
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // Determine navbar background based on path and scroll position
+  const navbarClass = location.pathname === '/' && !hasScrolled ? 'bg-transparent' : 'bg-black';
+
   return (
-    <div className={`sticky top-0 z-[1000] w-full transition-all duration-200 ${hasScrolled ? 'bg-black' : 'bg-transparent'}`}>
+    <div className={`sticky top-0 z-[1000] w-full transition-all duration-200 p-5 ${navbarClass}`}>
       <div className='flex justify-between items-center px-4 py-2 text-white'>
         <h1 className='text-xl'>ANUP R. GHEEWALA, MD</h1>
         <button onClick={toggleMenu} className="text-white">
@@ -33,15 +43,13 @@ const Navbar = () => {
             <XMarkIcon className="h-8 w-8 text-white" />
           </button>
           <div className="flex flex-col items-center justify-center space-y-8">
-            <a href="#home" className="text-white text-2xl">Home</a>
-            <a href="#about-us" className="text-white text-2xl">About Us</a>
+            <Link to="/" className="text-white text-2xl">Home</Link>
+            <Link to="/about" className="text-white text-2xl">About Us</Link>
             <a href="#breast" className="text-white text-2xl">Breast</a>
             <a href="#body" className="text-white text-2xl">Body</a>
             <a href="#face" className="text-white text-2xl">Face</a>
-            {/* Add more links as needed */}
           </div>
           <div className="pb-8">
-            {/* Place for potential bottom links or icons */}
           </div>
         </nav>
       </div>
@@ -50,5 +58,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
 
